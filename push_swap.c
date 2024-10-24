5d24cc9446616a43b72863c1be7536ec563ba69a
Bryan <89350478+sabahbryan@users.noreply.github.com>
2024-10-24 23:14:43 +0800

    Add files via upload

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: bryaloo <bryaloo@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 15:26:47 by bryaloo           #+#    #+#             */
-/*   Updated: 2024/10/16 22:44:17 by bryaloo          ###   ########.fr       */
+/*   Updated: 2024/10/24 17:37:38 by bryaloo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -325,10 +325,62 @@ static	void	set_target_a(t_stack_node *a, t_stack_node *b)
 		current_b = b;
 		while (current_b)
 		{
-			if
+			if (current_b->nbr < a->nbr && current_b->nbr > best_match_index)
+			{
+				best_match_index = current_b->nbr;
+				target_node = current_b;
+			}
+			current_b = current_b->next;
 		}
+		if (best_match_index == LONG_MIN)
+			a->target_node = find_max(b);
+		else
+			a->target_node = target_node;
+		a = a->next;
 	}
 }
+
+static void	cost_analysis_a(t_stack_node *a, t_stack_node *b)
+{
+	int	len_a;
+	int	len_b;
+	
+	len_a = stack_len(a);
+	len_b = stack_len(b);
+	while (a)
+	{
+		a->push_cost = a->index;
+		if (!(a->above_median))
+			a->push_cost = len_a - (a->index);
+		if (a->target_node->above_median)
+			a->push_cost += a->target_node->index;
+		else
+			a->push_cost += len_b - (a->target_node->index);
+		a = a->next;
+	}
+}
+
+void	set_cheapest(t_stack_node *stack)
+{
+	t_stack_node	*cheapest_node;
+	long			cheapest_cost;
+
+	if (!stack)
+		return ;
+	cheapest_value = LONG_MAX;
+	while (stack)
+	{
+		if (stack->push_cost < cheapest_value)
+		{
+			cheapest_value = stack->push_cost;
+			cheapest_node = stack;
+		}
+		stack = stack->next;
+	}
+	cheapest_node->cheapest = true;
+}
+
+static void move_a_to_b()
 
 //sudo apt install [valgrind]
 //valgrind --v
